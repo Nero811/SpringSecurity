19e4c34477c936b1c4e583f3639c626ec2bb7d78
unknown <jy811005@gmail.com>
2024-04-14 23:51:13 +0800

    update unit test

---
+++ src/test/java/com/example/demo/movie_security/MovieSecurityApplicationTest.java
@@ -0,0 +1,4 @@
+package com.example.demo.movie_security;
+
+public class MovieSecurityApplicationTest {
+}
---
+++ src/test/java/com/example/demo/movie_security/controller/MemberControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.demo.movie_security.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.example.demo.movie_security.entity.MemberEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class MemberControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testRegister() throws Exception {
+
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setEmail("test1@gmail.com");
+        memberEntity.setPassword("111");
+        memberEntity.setName("Test 1");
+        memberEntity.setAge(30);
+
+        String json = objectMapper.writeValueAsString(memberEntity);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/register")
+                .header("Content-Type", "application/json")
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+
+        requestBuilder = MockMvcRequestBuilders
+                .post("/userLogin")
+                .with(httpBasic("test1@gmail.com", "111"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    void testUserLogin() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/userLogin")
+                .with(httpBasic("normal@gmail.com", "normal"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+    }
+}
